dfde0c0b4077c8b84248e38bc98203b389096206
mohsin raza <mohsin_r_007@outlook.com>
2021-08-24 22:43:18 +0500

    Implementation of Depency Injection Design Pattern

---
+++ Implementations/CarService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo
+{
+    public class CarService : IService
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Car Service");
+        }
+    }
+}
